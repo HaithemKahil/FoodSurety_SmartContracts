116ed05a37591112dc4a9c49b97e0064913954d6
HaithemKahil <eh_kahil@esi.dz>
2018-08-03 06:58:09 +0200

    Smart Contract update state

--- FoodSafety_SmartContracts/contracts/MainContract.sol
+++ FoodSafety_SmartContracts/contracts/MainContract.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.4.0;
 
 contract MainContract {
-
-    
     struct userStruct {
         string userFullName;
         string userType;
@@ -10,7 +8,6 @@ contract MainContract {
         uint userAge;
         uint index ;
     }
-
 
     mapping (address => userStruct) private userStructs ;
     address[] private userList;
@@ -26,7 +23,6 @@ contract MainContract {
         } 
         return (userList[userStructs[userAddress].index] == userAddress);
     }
-
 
     function insertUser(address userAddress, string _userFullName, string _userType, string _userBrand, uint _userAge) public returns(uint index) {
         require(!isUser(userAddress)); 
@@ -90,4 +86,105 @@ contract MainContract {
         LogUpdateUser(userAddress, userStructs[userAddress].index, userStructs[userAddress].userFullName, userStructs[userAddress].userType, userStructs[userAddress].userBrand, userStructs[userAddress].userAge);
         return rowToDelete;
     }
+
+    struct Product {
+        string productName;
+        address productManufacturer;
+        address productOwner;
+        uint productQuantity;
+        address[] fromPath;
+        address[] toPath;
+        string[] positionPath;
+        string[] timePath;
+        string[] messagePath;
+        string[] maxt;
+        string[] mint;
+        uint index;
+    }
+
+    mapping (uint => Product) private productStructs; //information about the products
+    uint[] private productList; // the products' ids
+    uint private productCount ; // to keep track of the products ids
+
+    function isProduct(uint _productId) public view returns(bool isIndeed) {
+        if (productList.length == 0) {
+            return false;
+        } 
+        return (productList[productStructs[_productId].index] == _productId);
+    }
+
+    function insertProduct(string _productName, uint _productQuantity ) public returns(uint index) {
+        require(!isProduct(productCount)); 
+        productStructs[productCount].productName = _productName;
+        productStructs[productCount].productOwner = msg.sender;
+        productStructs[productCount].productManufacturer = msg.sender;
+        productStructs[productCount].productQuantity = _productQuantity;
+        productStructs[productCount].fromPath = new address[](16);
+        productStructs[productCount].toPath = new address[](16);
+        productStructs[productCount].positionPath = new string[](16);
+        productStructs[productCount].timePath = new string[](16);
+        productStructs[productCount].messagePath = new string[](16);
+        productStructs[productCount].maxt = new string[](16);
+        productStructs[productCount].mint = new string[](16);
+        productStructs[productCount].index = productList.push(productCount)-1;
+        productCount = productCount + 1;
+        //LogNewUser(userAddress, userStructs[userAddress].index, _userFullName, _userType, _userBrand, _userAge);
+        return productList.length-1;
+    }
+
+    function getProduct(uint _productId) public view returns (string _productName, uint _productQuantity, address _productManufacturer, string _productOwnerBrand) {
+        require(isProduct(_productId));
+        return (productStructs[_productId].productName, productStructs[_productId].productQuantity, productStructs[_productId].productManufacturer, userStructs[productStructs[_productId].productOwner].userBrand) ;
+    }
+
+    function getProductCount() public view returns(uint count) {
+        return productList.length ;
+    }
+
+    function getProductAtIndex(uint index) public view returns(uint productId) {
+        return productList[index] ;
+    }
+    
+    function addCheckPoint(uint _productId, address _receiver, string _position, string _time, string _message) public returns(bool success) {
+        require(msg.sender == productStructs[_productId].productOwner);
+        productStructs[_productId].fromPath.push(msg.sender);
+        productStructs[_productId].toPath.push(_receiver);
+        productStructs[_productId].positionPath.push(_position);
+        productStructs[_productId].timePath.push(_time);
+        productStructs[_productId].messagePath.push(_message);
+        productStructs[_productId].productOwner = _receiver;
+        return true;
+    }
+
+    // function getPath(uint _productId) public view returns (Action[]) {
+    //     uint length = productStructs[_productId].path.length;
+    //     Action[] memory path = new Action[](16);
+    //     for (uint i = 0; i < length; i++) {
+    //         path[i] = productStructs[_productId].path[i];
+    //     }
+    //     return path;
+    // }
+
+    function getActorsAtIndex(uint _productId, uint index) public view returns(address, address) {
+        return (productStructs[_productId].fromPath[index], productStructs[_productId].toPath[index]);
+    }
+
+    function getPathAtIndex(uint _productId, uint index) public view returns( string, string, string , string , string ) {
+        return ( productStructs[_productId].positionPath[index], productStructs[_productId].timePath[index], productStructs[_productId].maxt[index], productStructs[_productId].mint[index], productStructs[_productId].messagePath[index]);
+    }
+
+    function getUserProducts(address userId) public view returns(uint[]) {
+        uint[] results;
+        for(uint i = 0; i<productList.length;i++) {
+            if(productStructs[productList[i]].productOwner == userId) {
+                results.push(productList[i]);
+            }
+        }
+        uint[] memory staticArray = new uint[] (results.length);
+        for(uint j = 0; j < staticArray.length;j++ ) {
+              staticArray[j] = results[j];
+        }
+        return staticArray;
+    }
+
 }
